97b9f2db3f8144f2b2ec510e8f3d4d9140ae594c
Krishanu Singh <krishanu230@hotmail.com>
2023-02-25 15:56:57 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,31 @@
-#!/bin/sh -uex
-echo "Knowl script running to review linked documents..."
-ls
-echo "--"
-env
+#!/bin/bash
+
+if [ -z "$1" ]                # No argument passed?
+then
+    echo 'no filename provided'
+    exit 0
+fi
+
+
+filename_input=${1}
+result_dir=${2:-'results'}
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+echo $filename_input
+echo $result_dir
+if [[ ! -e $result_dir ]]; then
+    mkdir $result_dir
+elif [[ ! -d $result_dir ]]; then
+    echo "$result_dir already exists but is not a directory"
+else
+    echo "$result_dir already exists"
+    cd $result_dir
+    rm -vr *.html
+fi
+
+echo "---[1. running code_to_doc python script]---"
+cd ${SCRIPTPATH}
+python3.9 code_to_doc/python_docs.py -p $filename_input -o $result_dir
+
+echo "---[2. running importer script]---"
+ts-node importer/src/index.ts $result_dir -o $result_dir
